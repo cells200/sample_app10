8e422a7bf41d255ac6d6652780df2f3ea289e5a2
Simon Ng <simonn@gmail.com>
2012-06-23 19:24:47 -0700

    Updating users 20120623

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
-gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'bootstrap-sass', '~> 2.0.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
@@ -13,8 +13,8 @@ group :development, :test do
 end
 
 group :assets do
-  gem 'sass-rails',   '3.2.4'
-  gem 'coffee-rails', '3.2.2'
+  gem 'sass-rails',   '~> 3.2.4'
+  gem 'coffee-rails', '~> 3.2.2'
   gem 'uglifier', '1.2.3'
 end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "User page" do
+describe "User pages" do
 
   subject { page }
 
@@ -51,5 +51,23 @@ describe "User page" do
         it { should have_link('Sign out') }
       end
     end
-  end  
+  end
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+  end
+      
 end
